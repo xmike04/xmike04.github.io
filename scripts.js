cdbdcece662e73195524a0f3ced4c8207b2f8af4
xmike04 <miked24977@gmail.com>
2023-05-05 02:28:13 -0500

    Commit Message

--- scripts.js
+++ scripts.js
@@ -1,1 +1,7 @@
+function toggleMenu() {
+    const menuToggle = document.querySelector('.menu-toggle');
+    const navMenu = document.querySelector('.nav-menu');
 
+    menuToggle.classList.toggle('active');
+    navMenu.style.right = navMenu.style.right === '0px' ? '-300px' : '0px';
+}
