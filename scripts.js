453c55110555a76a0d2876bd2d75b4687ab4d3ac
xmike04 <miked24977@gmail.com>
2023-05-05 03:24:56 -0500

    Commit Message

--- scripts.js
+++ scripts.js
@@ -5,3 +5,37 @@ function toggleMenu() {
     menuToggle.classList.toggle('active');
     navMenu.style.right = navMenu.style.right === '0px' ? '-300px' : '0px';
 }
+
+const typedTextSpan = document.querySelector(".typed-text");
+const cursorSpan = document.querySelector(".cursor");
+
+const textArray = ["software engineer.", "web developer.", "student."];
+const typingDelay = 200;
+const erasingDelay = 100;
+const newTextDelay = 2000;
+let textArrayIndex = 0;
+let charIndex = 0;
+
+function type() {
+    if (charIndex < textArray[textArrayIndex].length) {
+        typedTextSpan.textContent += textArray[textArrayIndex].charAt(charIndex);
+        charIndex++;
+        setTimeout(type, typingDelay);
+    } else {
+        setTimeout(erase, newTextDelay);
+    }
+}
+
+function erase() {
+    if (charIndex > 0) {
+        typedTextSpan.textContent = textArray[textArrayIndex].substring(0, charIndex - 1);
+        charIndex--;
+        setTimeout(erase, erasingDelay);
+    } else {
+        textArrayIndex++;
+        if (textArrayIndex >= textArray.length) textArrayIndex = 0;
+        setTimeout(type, typingDelay + 1100);
+    }
+}
+
+setTimeout(type, newTextDelay + 250);
